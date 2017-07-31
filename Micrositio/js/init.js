70c3437cb06fb72dff29dbc6b38cf7b26dd9d8fd
Micropusheen <nancygarciadcv@gmail.com>
2017-07-31 14:47:42 -0500

    Update init.js

--- Micrositio/js/init.js
+++ Micrositio/js/init.js
@@ -23,7 +23,36 @@
   });
 
 
-    // Finds all iframes from youtubes and gives them a unique class
+    
+<!-- DIVISIÓN DE CIENCIAS SOCIALES-->
+ $(document).ready(function(){
+    // the "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered
+    $('.modal').modal();
+  });
+  
+  $('#modal1 #modal2 #modal3 #modal4 #modal5 #modal6').modal('open');
+
+
+   $('#modal1 #modal2 #modal3 #modal4 #modal5 #modal6').modal('close');       
+
+
+<!-- DIVISIÓN DE CIENCIAS FÍSICO-MATEMÁTICAS Y DE LAS INGENIERÍAS-->
+ $(document).ready(function(){
+    // the "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered
+    $('.modal').modal();
+  });
+  
+  $('#modal7 #modal8 #modal9 #modal10 #modal11 #modal12').modal('open');
+
+
+    $('.collapsible').collapsible({
+    accordion: false, // A setting that changes the collapsible behavior to expandable instead of the default accordion style
+    onOpen: function(el) { alert('Open'); }, // Callback for Collapsible open
+    onClose: function(el) { alert('Closed'); } // Callback for Collapsible close
+  });
+
+
+// Finds all iframes from youtubes and gives them a unique class
     jQuery('iframe[src*="https://www.youtube.com/embed/1nTqqc3WaHM"]').addClass("youtube-iframe");
 
 
