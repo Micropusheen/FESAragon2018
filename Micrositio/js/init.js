ba629e100467eacc3eb1ee497ad175eefa61aa88
Micropusheen <nancygarciadcv@gmail.com>
2017-07-31 14:45:50 -0500

    Update init.js

--- Micrositio/js/init.js
+++ Micrositio/js/init.js
@@ -21,3 +21,19 @@
        $(document).ready(function(){
     $('.collapsible').collapsible();
   });
+
+
+    // Finds all iframes from youtubes and gives them a unique class
+    jQuery('iframe[src*="https://www.youtube.com/embed/1nTqqc3WaHM"]').addClass("youtube-iframe");
+
+
+
+    jQuery(".fancybox-close").click(function() {
+      // changes the iframe src to prevent playback or stop the video playback in our case
+      $('.youtube-iframe').each(function(index) {
+        $(this).attr('src', $(this).attr('src'));
+        return false;
+      });
+      
+//click function
+    });
